a16e9974484cbb9e37765b1d5991ef811cab30c7
Vlad-2109 <vladswim21@gmail.com>
2024-05-22 20:07:15 +0300

    create the header section

--- client/src/router/router.tsx
+++ client/src/router/router.tsx
@@ -5,30 +5,19 @@ import { SignIn } from '../pages/SignIn';
 import { SignUp } from '../pages/SignUp';
 import { Dashboard } from '../pages/Dashboard';
 import { Projects } from '../pages/Projects';
+import { Layout } from '../components/Layout';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/about',
-    element: <About />,
-  },
-  {
-    path: '/sign-in',
-    element: <SignIn />,
-  },
-  {
-    path: '/sign-up',
-    element: <SignUp />,
-  },
-  {
-    path: '/dashboard',
-    element: <Dashboard />,
-  },
-  {
-    path: '/projects',
-    element: <Projects />,
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'about', element: <About /> },
+      { path: 'sign-in', element: <SignIn /> },
+      { path: 'sign-up', element: <SignUp /> },
+      { path: 'dashboard', element: <Dashboard /> },
+      { path: 'projects', element: <Projects /> },
+    ],
   },
 ]);
