4bb2546025358d4336e8f056b4586af38f15a57a
Vlad-2109 <vladswim21@gmail.com>
2024-05-22 20:59:25 +0300

    connect to the db

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const mongo_uri = process.env.MONGO_URI || '';
+const port = process.env.PORT || 3000;
+
+mongoose
+  .connect(mongo_uri)
+  .then(() => {
+    console.log('MongoDB is connected');
+  })
+  .catch((err) => console.log('DB error', err));
 
 const app = express();
 
-app.listen(3000, () => {
-	console.log('Server is running on port 3000');
+app.listen(port, () => {
+  console.log('Server is running on port 3000');
 });
