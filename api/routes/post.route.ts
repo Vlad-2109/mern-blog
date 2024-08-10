a752ecc5226e79b7bb17ef7ffba0cea47d81b3a1
Vlad-2109 <vladswim21@gmail.com>
2024-08-10 18:55:00 +0300

    create get posts api route

--- api/routes/post.route.ts
+++ api/routes/post.route.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { verifyToken } from '../utils/verifyUser';
-import { create } from '../controllers/post.controller';
+import { create, getPosts } from '../controllers/post.controller';
 
 const router = Router();
 
 router.post('/create', verifyToken, create);
+router.get('/get-posts', getPosts);
 
 export default router;
